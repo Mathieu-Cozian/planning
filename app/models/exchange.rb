55c0112a7bac47eb39e9746cd2d098abafb2947f
Mathieu-Cozian <mathieu.cozian@ieseg.fr>
2023-10-07 17:13:28 +0200

    Fixed bugs on index - Timeslots index backend done

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,5 +1,5 @@
 class Exchange < ApplicationRecord
-belongs_to :user_1, class_name: 'User'
-belongs_to :user_2, class_name: 'User'
-belongs_to :booking_2, class_name: 'Booking'
+belongs_to :user_1, class_name: 'User', dependent: :destroy
+belongs_to :user_2, class_name: 'User', dependent: :destroy
+belongs_to :booking_2, class_name: 'Booking', dependent: :destroy
 end
