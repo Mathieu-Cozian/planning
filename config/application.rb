1737ab4530ccca0e440cac8cdec4a00e7ef5875b
Mathieu-Cozian <mathieu.cozian@ieseg.fr>
2023-10-08 14:33:05 +0200

    Temporarly fixed the translation bug on the week calendar month display

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 module Planning
   class Application < Rails::Application
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
     config.i18n.available_locales = [:fr]
     config.i18n.default_locale = :fr
     config.generators do |generate|
